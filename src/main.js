df61ebb7563c7ce2638eeee170a309f76c1d70c9
Birgit Plakso <birgit.plakso@gmail.com>
2021-05-15 20:18:16 +0300

    töötav front

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+import VueAxios from "vue-axios";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { LayoutPlugin } from 'bootstrap-vue'
+// This imports the dropdown and table plugins
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+import { CardPlugin } from 'bootstrap-vue'
+import { ModalPlugin } from 'bootstrap-vue'
+import { VBScrollspyPlugin } from 'bootstrap-vue'
 
+Vue.use(VBScrollspyPlugin)
+
+Vue.use(ModalPlugin)
+Vue.use(CardPlugin)
+
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
+
+Vue.use(LayoutPlugin)
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to Loan Decision Engine</h1>
+    <br/>
+    <h3>Request for loan </h3>
+
+    <table style="width:100%" align="right">
+      <tr>
+        <th>Amount from 2000 EUR to 10000 EUR:</th>
+      </tr>
+      <tr>
+        <td><input v-model="requestedAmount" placeholder="Enter loan amount"></td>
+      </tr>
+      <tr>
+        <th>Loan period can be from 12 to 60 months</th>
+      </tr>
+      <tr>
+        <td><input v-model="requestedPeriod" placeholder="Enter period in months"></td>
+      </tr>
+      <tr>
+        <th>Choose customer code:</th>
+      </tr>
+      <tr>
+        <td>
+          <b-form-select v-model="selected" :options="options"></b-form-select>
+        </td>
+      </tr>
+    </table>
+    <br/>
+
+    <div>
+      <b-button variant="success" v-on:click="requestLoan()">Send request</b-button>
+    </div>
+    <br/>
+    <p v-if="errorMessage" class="regular-text">{{ errorMessage }}</p>
+    <p v-if="loanResponse" class="regular-text" >
+      Your requested loan is {{ loanResponse.enteredAmount }} EUR for {{ loanResponse.enteredPeriod }}
+      month period. <br/>
+    <p v-if="loanResponse && loanResponse.proposedAmount!=0"> The maximum amount for requested period
+      is {{ loanResponse.proposedAmount }} EUR </p>
+    <p v-if="loanResponse && loanResponse.proposedPeriod!=0"> We could provide the requested loan for
+      {{ loanResponse.proposedPeriod }} month period </p>
+
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      selected: null,
+      options: [
+        {value: null, text: 'Customer codes by segment:'},
+        {value: '49002010965', text: '49002010965'},
+        {value: '49002010976', text: '49002010976'},
+        {value: '49002010987', text: '49002010987'},
+        {value: '49002010998', text: '49002010998'}
+      ],
+      'requestedAmount': '',
+      'requestedPeriod': '',
+      'loanResponse': '',
+      'errorMessage': '',
+
+    }
+  },
+  methods: {
+    'requestLoan': function () {
+      if (this.requestedPeriod < 12 || this.requestedPeriod > 60) {
+        this.errorMessage = 'Loan period is out of bounds';
+        return;
+      } else if (this.requestedAmount < 2000 || this.requestedAmount > 10000) {
+        this.errorMessage = 'Loan amount is out of bounds';
+        return;
+      }
+      this.$http.get('api/loanDecision/' + this.requestedAmount
+          + '/' + this.requestedPeriod + '/' + this.selected)
+          .then(response => {
+            console.log(response);
+            this.loanResponse = response.data
+          })
+          .catch(response =>
+                  this.errorMessage = 'Error! Customer has dept, can not process loan request!'
+              // alert('juhtus viga')
+          )
+    }
   }
 }
 </script>
+
+<style>
+
+#button {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  font-color: #42b983;
+}
+
+</style>
